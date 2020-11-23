3cc33cdb179e57135e543a65fa9c305fe758b10a
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2020-11-23 18:56:29 +0000

    added list encoding and decoding

--- register_machine.lisp
+++ register_machine.lisp
@@ -7,5 +7,12 @@
         ((/= (mod y 2) 0) (values x (/ (- y 1) 2)))))
 
 (defun list-encode(l)
-    (match l 
-        (cons x xs)))
+    (if (null l) 
+        0 
+        (pair-encode (car l) (list-encode (cdr l)))))
+
+(defun list-decode(n)
+    (if (= n 0)
+        nil
+        (multiple-value-bind (x xs) (pair-decode n) 
+            (cons x (list-decode xs)))))
