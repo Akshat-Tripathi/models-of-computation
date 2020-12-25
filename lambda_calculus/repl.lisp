daecce1556a19768e606754696ffb9b22f497603
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2020-12-25 14:59:58 +0000

    Added repl.lisp

---
+++ lambda_calculus/repl.lisp
@@ -0,0 +1,45 @@
+(ql:quickload "str")
+(use-package :str)
+(load "core.lisp"  :external-format :utf-8)
+(load "types.lisp"  :external-format :utf-8)
+
+;;Register constants
+(defparameter constant-to-λ (make-hash-table))
+(defparameter λ-to-constant (make-hash-table))
+
+(defun register-constant(constant λ)
+ (setf (gethash λ λ-to-constant) constant)
+ (setf (gethash constant constant-to-λ) λ))
+
+(register-constant 'pair pair)
+(register-constant 'fst fst)
+(register-constant 'snd snd)
+
+(register-constant 'nil nil-λ)
+(register-constant 'cons cons-λ)
+
+(register-constant 'succ succ)
+(register-constant 'add add-λ)
+(register-constant 'mult mult)
+
+(register-constant 'true true)
+(register-constant 'false false)
+(register-constant 'if if-λ)
+(register-constant 'and and-λ)
+(register-constant 'or or-λ)
+(register-constant 'not not-λ)
+
+(defun replace-constants(expr)
+  (switch-expr-restore-brkt expr
+                            (let ((val (gethash expr constant-to-λ)))
+                              (if val
+                                val
+                                expr))
+                            (to-λ arg (replace-constants exp))
+                            (map 'list #'replace-constants expr)))
+
+(defun read-expr-from-string(str)
+  (read-from-string
+   (format nil "(~a)" ;Bracket expr
+           (replace-all "  " " " ;Remove all double spaces (also allows . without spaces)
+                        (replace-all "." " |.| " str))))) ;Escape dots
